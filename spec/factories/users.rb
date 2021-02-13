ac4d662e78549747af81cb99756b1bd9c945faba
usagrammer <uny0102@gmail.com>
2021-02-13 19:39:42 +0900

    commit

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    nickname {Faker::Name.last_name.slice(1..6)}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-  end
-end
