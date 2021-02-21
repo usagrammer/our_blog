cc55118e3f00b2a5b5510f714eb96c188bd52b2d
usagrammer <uny0102@gmail.com>
2021-02-21 16:45:38 +0900

    shared_contextではなくサポートモジュールに変更

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -7,69 +7,14 @@ RSpec.describe 'ログインしている', type: :system do
     @post = FactoryBot.build(:post, user: @user)
   end
 
-  ########################
-
-  shared_context '@post.userでログイン' do
-    before do
-      # トップページに移動する
-      visit root_path
-      # トップページにログインページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('ログイン')
-      # ログインページへ遷移する
-      visit new_user_session_path
-      # 正しいユーザー情報を入力する
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      # ログインボタンを押す
-      find('input[name="commit"]').click
-      # トップページへ遷移することを確認する
-      expect(current_path).to eq(root_path)
-      # カーソルを合わせるとログアウトボタンが表示されることを確認する
-      expect(
-        find('.header__right').find('a.header__right--btn[href="/users/sign_out"]')
-      ).to have_content('ログアウト')
-      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
-      expect(page).to have_no_content('新規登録')
-      expect(page).to have_no_content('ログイン')
-    end
-
-  end
-
-  shared_context '新規投稿ページへアクセス' do
-    before do
-      # 新規投稿ページに移動する
-      visit new_post_path
-    end
-  end
-
-  shared_context '詳細ページへアクセス' do
-    before do
-      @post.save
-      @new_title = 'bar'
-      @new_content = 'foo'
-
-      visit root_path
-      # 投稿済みのpostをクリックする
-      find('a.content__right__top--title:first-of-type').click
-      expect(page).to have_content(@post.title)
-    end
-  end
-
-  shared_context '編集ページへアクセス' do
-    before do
-      # 編集ページに移動する
-      find('.postManage__edit').click
-    end
-  end
-
-  ##################################
-
   context 'postが投稿できるとき' do 
 
-    include_context '@post.userでログイン'
-    include_context '新規投稿ページへアクセス'
-
     it '正しい情報を入力すればpostが投稿できてトップページに移動する' do
+      # ログイン
+      sign_in(@user)
+      # 新規投稿ページに移動する
+      visit new_post_path
+
       # フォームに情報を入力する
       fill_in 'post_title', with: @post.title
       fill_in 'post_content', with: @post.content
@@ -91,12 +36,17 @@ RSpec.describe 'ログインしている', type: :system do
       # 詳細ページには先ほど投稿したポストのcontentが存在する
       expect(page).to have_content(@post.content)
     end
+
   end
 
   context 'postが投稿できないとき' do
 
-    include_context '@post.userでログイン'
-    include_context '新規投稿ページへアクセス'
+    before do
+      # ログイン
+      sign_in(@user)
+      # 新規投稿ページに移動する
+      visit new_post_path
+    end
 
     it 'titleが不足していると投稿できない' do
       # フォームに情報を入力する
@@ -122,11 +72,27 @@ RSpec.describe 'ログインしている', type: :system do
 
   context 'postが編集できるとき' do
 
-    include_context '@post.userでログイン'
-    include_context '詳細ページへアクセス'
-    include_context '編集ページへアクセス'
+    before do
+      # ログイン
+      sign_in(@user)
+
+      # 詳細ページへ移動する
+      @post.save
+
+      visit root_path
+      # 投稿済みのpostをクリックする
+      find('a.content__right__top--title:first-of-type').click
+      expect(page).to have_content(@post.title)
+
+      # 編集ページへアクセス
+      find('.postManage__edit').click
+      # 編集内容
+      @new_title = 'bar'
+      @new_content = 'foo'
+    end
 
     it '正しい情報を入力すればpostが編集できてトップページに移動する' do
+
       # フォームに情報を入力する
       fill_in 'post_title', with: @new_title
       fill_in 'post_content', with: @new_content
@@ -144,9 +110,25 @@ RSpec.describe 'ログインしている', type: :system do
 
   context 'postが編集できないとき' do 
 
-    include_context '@post.userでログイン'
-    include_context '詳細ページへアクセス'
-    include_context '編集ページへアクセス'
+    before do
+      # ログイン
+      sign_in(@user)
+
+      # 詳細ページへアクセス
+      @post.save
+
+      visit root_path
+      # 投稿済みのpostをクリックする
+      find('a.content__right__top--title:first-of-type').click
+      expect(page).to have_content(@post.title)
+
+      # 編集ページへアクセス
+      find('.postManage__edit').click
+
+      # 編集内容
+      @new_title = 'bar'
+      @new_content = 'foo'
+    end
 
     it 'titleが不足していると更新できない' do
       # フォームに情報を入力する
@@ -171,8 +153,19 @@ RSpec.describe 'ログインしている', type: :system do
   end
 
   context 'postが削除できるとき' do 
-    include_context '@post.userでログイン'
-    include_context '詳細ページへアクセス'
+
+    before do
+      # ログイン
+      sign_in(@user)
+
+      # 詳細ページへアクセス
+      @post.save
+
+      visit root_path
+      # 投稿済みのpostをクリックする
+      find('a.content__right__top--title:first-of-type').click
+      expect(page).to have_content(@post.title)
+    end
 
     it '投稿者なら自分のpostを削除できる' do
       # 削除ボタンをクリック
@@ -185,8 +178,19 @@ RSpec.describe 'ログインしている', type: :system do
   end
 
   context 'postが削除できないとき' do
-    include_context '@post.userでログイン'
-    include_context '詳細ページへアクセス'
+
+    before do
+      # ログイン
+      sign_in(@user)
+
+      # 詳細ページへアクセス
+      @post.save
+
+      visit root_path
+      # 投稿済みのpostをクリックする
+      find('a.content__right__top--title:first-of-type').click
+      expect(page).to have_content(@post.title)
+    end
 
     it '投稿者でないなら自分のpostを削除できない' do
       ## @postのuserを別のuserにしておく
@@ -199,6 +203,7 @@ RSpec.describe 'ログインしている', type: :system do
         find('.postManage__delete').click
       }.not_to change { Post.count }
     end
+
   end
 
   context 'ログインしていない' do
@@ -219,7 +224,16 @@ RSpec.describe 'ログインしている', type: :system do
     end
 
     context 'postが編集できないとき' do
-      include_context '詳細ページへアクセス'
+
+      before do
+        # 詳細ページへアクセス
+        @post.save
+
+        visit root_path
+        # 投稿済みのpostをクリックする
+        find('a.content__right__top--title:first-of-type').click
+        expect(page).to have_content(@post.title)
+      end
 
       it 'ログインしていないと編集ボタンが表示されない' do
         expect(page).to have_no_content('編集')
@@ -235,7 +249,16 @@ RSpec.describe 'ログインしている', type: :system do
     end
 
     context 'postが削除できないとき' do
-      include_context '詳細ページへアクセス'
+
+      before do
+        # 詳細ページへアクセス
+        @post.save
+
+        visit root_path
+        # 投稿済みのpostをクリックする
+        find('a.content__right__top--title:first-of-type').click
+        expect(page).to have_content(@post.title)
+      end
 
       it 'ログインしていないと削除ボタンが表示されない' do
         expect(page).to have_no_content('削除')
